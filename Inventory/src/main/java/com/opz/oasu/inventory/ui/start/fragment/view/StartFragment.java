71226734be28af377bdc18f19df23c7b9787e369
Kucherenko Nikita <phobos-nik@users.noreply.github.com>
2018-01-02 08:53:32 +0200

    Added inventory control buttons to details layouts.

--- Inventory/src/main/java/com/opz/oasu/inventory/ui/start/fragment/view/StartFragment.java
+++ Inventory/src/main/java/com/opz/oasu/inventory/ui/start/fragment/view/StartFragment.java
@@ -2,9 +2,7 @@ package com.opz.oasu.inventory.ui.start.fragment.view;
 
 import android.app.FragmentTransaction;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
 import android.support.annotation.Nullable;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,11 +11,6 @@ import com.opz.oasu.inventory.R;
 import com.opz.oasu.inventory.ui.common.view.fragment.BaseViewFragment;
 import com.opz.oasu.inventory.ui.details.fragment.view.DetailsFragment;
 import com.opz.oasu.inventory.ui.start.fragment.presenter.StartPresenter;
-
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
 
 import javax.inject.Inject;
 
@@ -37,7 +30,7 @@ public class StartFragment extends BaseViewFragment<StartPresenter> implements S
         return layoutInflater.inflate(R.layout.fragment_start, container, false);
     }
 
-    @OnClick(R.id.fragment_start_start_new_inventory_button)
+    @OnClick(R.id.fragment_start_button_start_new_inventory)
     void onStartNewInventoryButtonClick() {
         presenter.createNewInventory();
 
